e964caf1f2cdeac5ef728eb383d619944f194172
StephMachado <steph_machado@hotmail.com>
2016-06-15 22:20:50 -0500

    Clase 15/06/2016

---
+++ WebDeveloper.DataAccess/ClientData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.DataAccess
+{
+    public class ClientData : BaseDataAccess<Client>
+    {
+        public Client GetClient(int id)
+        {
+            using (var dbContext = new WebContextDb())
+            {
+                return dbContext.Clients.FirstOrDefault(x => x.ID == id);
+
+                //Este método no debemos utilizarlo, ya que traemos todo el listado de los clientes.
+                //return this.GetList().Where(c => c.ID == id).FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ WebDeveloper/Controllers/ClientController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.DataAccess;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Controllers
+{
+    public class ClientController : Controller
+    {
+        private ClientData _client = new ClientData();
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            var client = new ClientData();
+            return View(_client.GetList());
+        }
+
+        //1° Crear Cliente
+        public ActionResult Create()
+        {
+            return View(new Client());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _client.Add(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //2° Eliminar Cliente
+        public ActionResult Delete(int id)
+        {
+            //Primera línea de código
+            //return View(_client.GetList().Where(c => c.ID == id).FirstOrDefault());
+            
+            //Segunda línea de código
+            //return View(_client.GetClientById(id));
+
+            //Código del profe
+            var client = _client.GetClient(id);
+            if (client== null)
+                RedirectToAction("Index");
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Client client)
+        {
+            _client.Delete(client);
+            return RedirectToAction("Index");
+
+            //Comentario
+            //if (ModelState.IsValid)
+            //[No se requiere esta línea de código, ya que no validamos nada]
+        }
+
+
+        //3° Editar Cliente
+        public ActionResult Edit(int id)
+        {
+            //Primera línea de código
+            //return View(_client.GetList().Where(c => c.ID == id).FirstOrDefault());
+
+            //Segunda línea de código
+            //return View(_client.GetClientById(id));
+
+            //Código del profe
+            var client = _client.GetClient(id);
+            if (client == null)
+                RedirectToAction("Index");
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _client.Update(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
